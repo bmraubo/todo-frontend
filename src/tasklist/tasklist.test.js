45b1ef220a749582ef7fcac353d0e8f72e436697
bmraubo <bmraubo@gmail.com>
2022-01-12 12:09:23 +0000

    tasklist test display all

--- src/tasklist/tasklist.test.js
+++ src/tasklist/tasklist.test.js
@@ -1,3 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import BackendMock from "../connector/backend-mock"
+import TaskList from "./tasklist"
+
 describe("Testing tasklist...", () => {
-    
+
+    it("on render, displays list of tasks", () => {
+        global.fetch = jest.fn(() => 
+        Promise.resolve({
+            json: () => Promise.resolve(BackendMock.mockTest())
+        }))
+
+        render(<TaskList />)
+
+        expect(screen.getByText("a final task")).toBeVisible()
+    })
 })
