2b6adf7f4f06aec30d53fb1646f9c35045ca3184
bmraubo <bmraubo@gmail.com>
2022-01-12 18:49:58 +0000

    connector uses 0.0.0.0 instead of localhost

--- src/connector/connector.js
+++ src/connector/connector.js
@@ -1,7 +1,7 @@
 export default class Connector {
 
     static async fetchAllTasks() {
-        let url = "localhost:5000/todos";
+        let url = "0.0.0.0:5000/todos";
 
         let response = await fetch(url);
         let response_json = await response.json();
