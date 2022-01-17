605c644fb4d1840959a686e1cc201debf8d9cc74
bmraubo <bmraubo@gmail.com>
2022-01-17 16:12:35 +0000

    handleSubmit returns new task object

--- src/add_task/add_task.jsx
+++ src/add_task/add_task.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function AddTask(props) {
     let taskInput = React.createRef();
 
-    function handleSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault()
-        let result = props.connector.addTask(taskInput.current.value)
+        let result = await props.connector.addTask(taskInput.current.value)
         console.log(result)
     }
     
