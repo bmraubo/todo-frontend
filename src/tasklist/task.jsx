3492e00b7ec1b7d3f12cd726de14b8f69a00a339
bmraubo <bmraubo@gmail.com>
2022-01-18 11:04:45 +0000

    added input box for edit

--- src/tasklist/task.jsx
+++ src/tasklist/task.jsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import "./tasklist.css"
 
 export default function Task(props) {
     const taskMessage = `Task: ${props.task.task} Status: ${DoneStatus(props.task.done)}`
+    const [editMode, setEditMode] = useState(false)
+    let taskInput = React.createRef();
 
     function Button(props) {
         return (
@@ -10,6 +12,22 @@ export default function Task(props) {
                 {props.message}
             </button>
         )
+    }
+
+    function InputBox(props) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder={props.task.task} ref={taskInput}></input>
+                    <button>Edit Task</button>
+                </form>
+            </div>
+        )
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        alert(taskInput.current.value)
     }
 
     async function changeDoneStatus() {
@@ -22,6 +40,14 @@ export default function Task(props) {
         props.getData()
     }
 
+    async function EditTask() {
+        setEditMode(!editMode)
+        // id, new task info, done status
+        // input for new task info
+        // edit will swap out old task info for new task info
+        // send new task to connector.editTask
+    }
+
     function DoneStatus(doneStatus) {
         if (doneStatus) {
             return "Completed!";
@@ -30,14 +56,19 @@ export default function Task(props) {
         }
     }
 
-    return (  
-        <div className="row">
-            <div className="column">
-                <Button testid={props.task.task} onClick={changeDoneStatus} message={taskMessage}/>
-            </div>
-            <div>
-                <Button testid={`Edit ${props.task.id}`} message="Edit"/>
-                <Button testid={`Delete ${props.task.id}`} onClick={DeleteTask} message="Delete"/>
+    return (
+        <div>
+        {editMode &&
+        <InputBox task={props.task} />
+        }  
+            <div className="row">
+                <div className="column">
+                    <Button testid={props.task.task} onClick={changeDoneStatus} message={taskMessage}/>
+                </div>
+                <div>
+                    <Button testid={`Edit ${props.task.id}`} onClick={EditTask} message="Edit"/>
+                    <Button testid={`Delete ${props.task.id}`} onClick={DeleteTask} message="Delete"/>
+                </div>
             </div>
         </div>
     )
