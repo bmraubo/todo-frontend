429b19bafbe7f62018528708bc4177719415731f
bmraubo <bmraubo@gmail.com>
2022-01-19 11:52:04 +0000

    Edit Task input placeholder fix

--- src/tasklist/InputBox.jsx
+++ src/tasklist/InputBox.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 
 export default function InputBox(props) {
-    const [taskInput, setTaskInput] = useState(" ");
+    const [taskInput, setTaskInput] = useState("");
 
     async function handleSubmit(event) {
         event.preventDefault()
